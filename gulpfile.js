d58e07534e22bd1803995d53b264aa1a9f9044c3
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-04-01 12:10:40 +0200

    get rid of favicon error

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 const gulp = require('gulp'),
   connect = require('gulp-connect');
  
-const runServer = () => {
-  connect.server({
-    root: 'app'
-  });
-}
+const runServer = () => connect.server();
 
 exports.default = runServer;
