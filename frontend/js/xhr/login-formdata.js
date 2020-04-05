01fc88e6a8426ad75233d0dbeb7d0153ab8a1dc9
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-04-05 09:24:05 +0200

    use FormData object to send xhr request

---
+++ frontend/js/xhr/login-formdata.js
@@ -0,0 +1,22 @@
+(function() {
+	'ust strict'
+
+	const form = document.getElementById('login');
+
+	function submitData(event) {
+		const xhr = new XMLHttpRequest(),
+			formData = new FormData(),
+			username = document.getElementById('username').value,
+			password = document.getElementById('password').value;
+
+		formData.append('username', username);
+		formData.append('password', password);
+
+		xhr.open('POST', '/signin');
+		xhr.send(formData);
+
+		event.preventDefault();
+	}
+
+	form.addEventListener('submit', submitData);
+})();
