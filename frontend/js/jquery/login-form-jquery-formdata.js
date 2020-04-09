face222550b71f82eebfab0388490d69fc5b6e65
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-04-09 08:24:11 +0200

    display respnse text send by backend - jquery

--- frontend/js/jquery/login-form-jquery-formdata.js
+++ frontend/js/jquery/login-form-jquery-formdata.js
@@ -1,6 +1,8 @@
 (function($){
 	'use strict';
 
+	const $confirmationMessage = $('.confirmation-message');
+
 	$('#login').on('submit', function(event) {
 		const $loginInputs = $(this).find('input'),
 			formData = new FormData();
@@ -16,6 +18,7 @@
 			processData: false, //stop jQuery from trying to add the FormData object to the query string of the URL
 			success: function(response) {
 				console.log(response);
+				$confirmationMessage.text(response.status)
 			}
 		});
 
